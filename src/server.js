ac9f191a172a31c7dbd54e26e8dbedea0665a966
Julian Campos <julian.ocampos@hotmail.com>
2025-11-21 19:11:52 -0300

    feat: defining env file

--- src/server.js
+++ src/server.js
@@ -2,6 +2,9 @@ const grpc = require('@grpc/grpc-js')
 const notesService = require('./notes/note-service')
 const usersService = require('./users/user-service')
 
+const PORT = process.env.PORT || 50051;
+const URL = process.env.URL || '127.0.0.1';
+
 const start = () => {
     const notesProto = grpc.loadPackageDefinition(notesService.loadNotesProto())
     const usersProto = grpc.loadPackageDefinition(usersService.loadUsersProto())
@@ -11,9 +14,9 @@ const start = () => {
     server.addService(usersProto.UserService.service, { List: usersService.List, Find: usersService.Find });
 
     server.bindAsync(
-        '127.0.0.1:50051',
+        `${URL}:${50051}`,
         grpc.ServerCredentials.createInsecure(), () => {
-            console.log('Server running at http://127.0.0.1:50051')
+            console.log(`Server running at http://${URL}:${PORT}`);
         }
     )
 }
